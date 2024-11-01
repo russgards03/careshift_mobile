b29eaf00af027da85449e57900de64d6c7924de2
Russ Allen Garde <99627318+russgards03@users.noreply.github.com>
2024-11-01 13:08:54 +0800

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -14,46 +14,73 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   String errorMessage = '';
 
   Future<void> _login() async {
-    final String email = emailController.text.trim();
-    final String password = passwordController.text.trim();
+  final String email = emailController.text.trim();
+  final String password = passwordController.text.trim();
+  const String apiUrl = 'https://russgarde03.helioho.st/serve/nurse/validate.php';
 
-    const String apiUrl = 'https://russgarde03.helioho.st/serve/nurse/validate.php';
+  try {
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      body: jsonEncode({'nurse_email': email, 'nurse_password': password}),
+      headers: {'Content-Type': 'application/json'},
+    );
 
-    try {
-      final response = await http.post(
-        Uri.parse(apiUrl),
-        body: jsonEncode({'nurse_email': email, 'nurse_password': password}),
-        headers: {'Content-Type': 'application/json'},
-      );
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}'); // Log the response body
 
-      if (response.statusCode == 201) {
-        final responseData = jsonDecode(response.body);
-        final bool validation = responseData['validation'];
+    if (response.statusCode == 201) {
+      final responseData = jsonDecode(response.body);
+      print('Response data: $responseData');
+      final bool validation = responseData['validation'];
 
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setBool('validation', validation);
-
-        if (validation) {
+      if (validation) {
+        String? nurseId = responseData['nurse_id'];
+        if (nurseId != null) {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString('nurse_id', nurseId);
+          print('Nurse ID saved: $nurseId');
           _navigateToHome();
         } else {
           setState(() {
-            errorMessage = 'Cannot verify account.';
+            errorMessage = 'Nurse ID not found.';
           });
         }
       } else {
         setState(() {
-          errorMessage = 'Invalid ID number or password';
+          errorMessage = 'Cannot verify account.';
         });
       }
-    } catch (e) {
-      setState(() {
-        errorMessage = 'Error: $e';
-      });
+    } else {
+      _handleErrorResponse(response);
+    }
+  } catch (e) {
+    setState(() {
+      errorMessage = 'Error: $e';
+    });
+  }
+}
+
+  void _handleErrorResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 400:
+        setState(() {
+          errorMessage = 'Invalid input. Please check your credentials.';
+        });
+        break;
+      case 401:
+        setState(() {
+          errorMessage = 'Unauthorized. Check your email and password.';
+        });
+        break;
+      default:
+        setState(() {
+          errorMessage = 'An unexpected error occurred. Please try again later.';
+        });
     }
   }
 
@@ -63,7 +90,6 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    TextStyle linkStyle = TextStyle(color: Colors.blue);
     return Scaffold(
       backgroundColor: AppColors.mainLightColor,
       body: SafeArea(
@@ -74,118 +100,113 @@ class _LoginPageState extends State<LoginPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(height: 50),
-                const Icon(
-                  Icons.medical_information,
-                  size: 100,
-                ),
-                Text(
-                  'CareShift',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 64,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Text(
-                  'A CLMMRH Nurse Scheduling Application',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextField(
-                        cursorColor: AppColors.mainDarkColor,
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color.fromARGB(255, 212, 208, 208)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
-                          ),
-                          fillColor: Colors.white,
-                          filled: true,
-                          hintText: 'ID Number',
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      TextField(
-                        cursorColor: AppColors.mainDarkColor,
-                        controller: passwordController,
-                        decoration: const InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color.fromARGB(255, 212, 208, 208)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
-                          ),
-                          fillColor: Colors.white,
-                          filled: true,
-                          hintText: 'Password',
-                        ),
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 20.0),
-                      Text(
-                        errorMessage,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                      const SizedBox(height: 20.0),
-                      GestureDetector(
-                        onTap: _login,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                          decoration: BoxDecoration(
-                            color: const Color(0xFF29CE7A),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 60.0),
-                      RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Don\'t have an account yet? ',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                            TextSpan(
-                              text: 'Sign Up',
-                              style: linkStyle,
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => RegisterPage()),
-                                  );
-                                },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                const Icon(Icons.medical_information, size: 100),
+                _buildTitle(),
+                _buildSubtitle(),
+                _buildLoginForm(),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Text(
+      'CareShift',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 64,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildSubtitle() {
+    return const Text(
+      'A CLMMRH Nurse Scheduling Application',
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 16,
+      ),
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            cursorColor: AppColors.mainDarkColor,
+            controller: emailController,
+            decoration: _inputDecoration('ID Number'),
+          ),
+          const SizedBox(height: 25),
+          TextField(
+            cursorColor: AppColors.mainDarkColor,
+            controller: passwordController,
+            decoration: _inputDecoration('Password'),
+            obscureText: true,
+          ),
+          const SizedBox(height: 20.0),
+          Text(errorMessage, style: const TextStyle(color: Colors.red)),
+          const SizedBox(height: 20.0),
+          _buildLoginButton(),
+          const SizedBox(height: 60.0),
+          _buildSignUpLink(),
+        ],
+      ),
+    );
+  }
+
+  InputDecoration _inputDecoration(String hintText) {
+    return InputDecoration(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: const Color.fromARGB(255, 212, 208, 208)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+      ),
+      fillColor: Colors.white,
+      filled: true,
+      hintText: hintText,
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return GestureDetector(
+      onTap: _login,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        decoration: BoxDecoration(
+          color: const Color(0xFF29CE7A),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: const Text(
+          'Login',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignUpLink() {
+    TextStyle linkStyle = const TextStyle(color: Colors.blue);
+    return RichText(
+      text: TextSpan(
+        children: <TextSpan>[
+          const TextSpan(text: 'Don\'t have an account yet? ', style: TextStyle(color: Colors.black)),
+          TextSpan(
+            text: 'Sign Up',
+            style: linkStyle,
+            recognizer: TapGestureRecognizer()..onTap = () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterPage()));
+            },
+          ),
+        ],
       ),
     );
   }
