4bdd25985861c1b64d5496ed56f27746dd2a3aa3
Russ Allen Garde <99627318+russgards03@users.noreply.github.com>
2024-11-02 00:52:50 +0800

    Delete colors.dart

--- colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color myColor = Color(0xFF181A1D);
-  static const Color mainDarkColor = Color(0xFF181A1D);
-  static const Color mainColor = Color(0xFF29CE7A);
-  static const Color mainLightColor = Color.fromARGB(255, 255, 255, 255);
-}
